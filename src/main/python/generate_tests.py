288bc9ea98e05205efd4639002d910a2fd245fcf
Grey Panther <gpanther@grey-panther.info>
2016-03-29 17:56:35 +0300

    Updated benchmarks

--- src/main/python/generate_tests.py
+++
@@ -1,9 +0,0 @@
-offsets = [0, 1, 2, 3, 7, 8, 9, 15, 16, 17, 21, 23, 24, 27, 29, 31]
-
-for i in offsets:
-	for j in offsets:
-		print """	@Benchmark
-	public final void arraycopy_%d_%d() {
-		System.arraycopy(source, %d, destination, %d, size);
-	}""" % (i, j, i, j)
-
--- src/main/python/json_to_table.py
+++ src/main/python/json_to_table.py
@@ -3,12 +3,19 @@ import json
 
 data = json.loads(open(sys.argv[1]).read())
 
+print "\t".join(["name", "min", "lowBound", "highBound", "max", "sourceOffset", "destinationOffset"])
+
 for benchmark in data:
 	name = benchmark['benchmark']
+	name = name.replace('net.greypanther.BenchmarkAlignment.', '')
+	src_offset = int(benchmark['params']['sourceOffset'])
+	dest_offset = int(benchmark['params']['destinationOffset'])
+	name = '%s_%d_%d' % (name, src_offset, dest_offset)
 	_min = benchmark['primaryMetric']['scorePercentiles']['0.0']
 	_max = benchmark['primaryMetric']['scorePercentiles']['100.0']
 	_avg = benchmark['primaryMetric']['score']
 	_error = benchmark['primaryMetric']['scoreError']
 
-	print "\t".join(str(v) for v in [name, _min, _avg - _error, _avg + _error, _max])
+	print "\t".join(str(v) for v in
+		[name, _min, _avg - _error, _avg + _error, _max, src_offset, dest_offset])
 
